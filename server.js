126765c34c77e70809714afa9addbf226dbd58b5
Uraeus Ray Millet <uraeusmillet@gmail.com>
2017-05-08 16:04:44 +0000

    add mongoose and validurl config

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
+// package for url validation
+const validUrl = require('valid-url');
+
+// mongodb uri
+const mongoUri = process.env.MONGOLAB_URI || 'mongodb://localhost/app-dev';
+const mongoOptions = {db: {safe: true}};
+const baseUrl = process.env.BASE_URL || ('http://localhost:8080/');
 app.get('/', function(req, res) {
     res.send("Hello World");
 })
 
 app.listen(8080, function() {
     console.log("Listening on port 8080");
-})
+});
