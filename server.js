89afceb4e451b42ec122ee426e186bbfa5f94607
Uraeus Ray Millet <uraeusmillet@gmail.com>
2017-05-08 16:54:59 +0000

    app connects to db

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ const validUrl = require('valid-url');
 const mongoUri = process.env.MONGOLAB_URI || 'mongodb://localhost/app-dev';
 const mongoOptions = {db: {safe: true}};
 const baseUrl = process.env.BASE_URL || ('http://localhost:8080/');
+
+// connect to database
+mongoose.connect(mongoUri, mongoOptions);
+// run error if connection fails
+mongoose.connection.on('error', function(err) {
+    console.error(`MongoDB connection failed due to ${err}`);
+    process.exit(-1);
+})
+
 app.get('/', function(req, res) {
     res.send("Hello World");
 })
